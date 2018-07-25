534880250a33442444a5f89d37a0e10fbd423c8b
James Tays <james@jaila-files.com>
2018-07-24 21:55:44 -0400

    Unit Testing Scientific Caculator

---
+++ SampleCalculator.ViewsModuleTests/ViewModels/ScientificCalculatorViewModelTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SampleCalculator.ViewsModule.ViewModels;
+
+namespace SampleCalculator.ViewsModuleTests.ViewModels
+{
+    [TestClass]
+    public class ScientificCalculatorViewModelTests
+    {
+        private ScientificCalculatorViewModel _scientificCalculatorViewModel;
+
+        public ScientificCalculatorViewModelTests()
+        {
+            _scientificCalculatorViewModel = new ScientificCalculatorViewModel();
+        }
+
+        [TestMethod]
+        public void SymbolPushedCommand_Exressions_ShouldWrapCurrentExpressionInPerenthesis()
+        {
+            //Arrange
+            _scientificCalculatorViewModel.Expression = "3*3";
+
+            //Act
+            _scientificCalculatorViewModel.SymbolButtonPushedCommand.Execute("Sqrt");
+
+            //Assert
+            Assert.AreEqual("Sqrt(3*3)", _scientificCalculatorViewModel.Expression);
+        }
+    }
+}
