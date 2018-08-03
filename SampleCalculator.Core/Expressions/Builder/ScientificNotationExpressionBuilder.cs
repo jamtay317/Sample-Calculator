56d004f44fa0459ca959e12bb9235baf10e7f4ef
James Tays <james@jaila-files.com>
2018-08-03 11:40:49 -0400

    Scientific Notation Working

---
+++ SampleCalculator.Core/Expressions/Builder/ScientificNotationExpressionBuilder.cs
@@ -0,0 +1,11 @@
+﻿namespace SampleCalculator.Core.Expressions.Builder
+{
+    public class ScientificNotationExpressionBuilder:IExpressionBuilder
+    {
+        public Expression Build(Expression expression, string symobl)
+        {
+            expression = $"{Constants.Expressions.Exponent}(10)^{expression}";
+            return expression;
+        }
+    }
+}
